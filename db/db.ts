37d05946b205d89668b89e4607da45cfb6c8ccb9
axl <axl.castilloblasco@gmail.com>
2024-10-30 13:14:14 -0400

    first commit

---
+++ db/db.ts
@@ -0,0 +1,11 @@
+import * as mysql from "https://deno.land/x/mysql@v2.12.1/mod.ts";
+import "jsr:@std/dotenv/load";
+
+export const client = new mysql.Client();
+
+client.connect({
+	hostname: Deno.env.get("beaver_host"),
+	username: Deno.env.get("beaver_user"),
+	db: Deno.env.get("beaver_db"),
+	password: Deno.env.get("beaver_pw"),
+});
