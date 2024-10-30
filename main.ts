ed8b5841ca7537243e3c5d011abb91f158756b3a
axl <axl.castilloblasco@gmail.com>
2024-10-30 17:57:02 -0400

    forgot to stage w/previous

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-import { client } from "./db/db.ts";
+import express from "npm:express@4.18.2";
+
+const app = express();
+
+app.get("/", (req: express.Request, res: express.Response) => {
+	res.send("hi");
+});
+
+app.listen(3000);
