79106c2b9377e534ef9b7805c418fd5a3e702394
axl <axl.castilloblasco@gmail.com>
2024-10-30 22:33:10 -0400

    basic routing and new files

--- main.ts
+++ main.ts
@@ -1,9 +1,14 @@
 import express from "npm:express@4.18.2";
+import { maeRouter } from "./db/maestranza/products.ts";
 
 const app = express();
+const port = Number(Deno.env.get("PORT")) || 8000;
+
 
 app.get("/", (req: express.Request, res: express.Response) => {
 	res.send("hi");
 });
 
-app.listen(3000);
+app.use("/maestranza", maeRouter);
+
+app.listen(port);
